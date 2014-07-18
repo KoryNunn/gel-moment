3bea2dbb4c38ee1e927dca917e5ded9e4ae02ea2
KoryNunn <knunn187@gmail.com>
2014-07-18 11:09:50 +1000

    Fixed standard case for dates

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var moment = require('moment');
 module.exports = function(scope){
     scope.date = function(scope, args){
         var all = args.all();
+        if(all.length === 1){
+            all = all.pop();
+        }
         return moment(all)._d;
     };
 
